00fc7d9b48b9e62550ecf14342c48a3225b27eda
clancyfogarty <clancyfogarty@gmail.com>
2017-11-09 19:18:02 -0800

    Menu done

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,2 +1,9 @@
-puts "Welcome to AddressBloc!"
+require_relative 'controllers/menu_controllers'
 
+## create new MenuController upon start
+menu = MenuController.new
+## clears command line
+system "clear"
+puts "Welcome to AddressBloc!"
+## calls main_menu to display the menu
+menu.main_menu
